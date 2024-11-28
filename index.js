1a10cec99e92b6bb1188dc1a22a17f84f90eeb04
NisathAndroid <nisathatadvantech@gmail.com>
2024-11-28 11:10:31 +0530

    init contact api

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const app = express();
+
+require("dotenv").config();
+
+app.use(express.json());
+
+const connectDB = require("./connectMongo");
+
+connectDB();
+
